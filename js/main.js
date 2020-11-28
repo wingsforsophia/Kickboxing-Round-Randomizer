932678970e20cfe718c0622d182f466e4ad8db2f
Sophia Seremetis <shseremetis@gmail.com>
2020-11-28 11:05:17 -0500

    round 2 generator working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // CONSTANTS--------------------------------------------------------------------------
 const round1 = {
-    'punches': ['Jab', 'Cross'],
+    'punches': ['Jab', 'Cross', 'Body-Cross', 'Body-Jab'],
     'combos': ['10 Jab-Cross, 20 Jab-Cross... up to 50 Jab-Cross, then 5 burpees', 
                 'Jab-Cross',
                 'Jab-Cross-Cross',
@@ -20,16 +20,31 @@ const round1 = {
                 'Jab,Cross, Jab-Cross, then Jab,Jab,Cross,Cross, then Jab-Cross-Jab-Cross, up to 5 individual, then 10 jumping jacks']
 }
 
+const round2 = {
+    'punches': ['Jab', 'Cross', 'Body-Cross', 'Body-Jab', 'Left-Hook', 'Right-Hook', 'Left-Body-Hook', 'Right-Body-Hook', 'Left-Uppercut', 'Right-Uppercut'],
+    'combos':  ['Jab, Left-Body-Hook, Right-Body-Hook, Left-Uppercut, Cross',
+                'Cross, Left-Hook, Body-Cross, Right-Hook',
+                'Cross, Left-Uppercut, Left-Hook, Right-Hook, Right-Spinning-Backfist',
+                'Right-Uppercut, Left-Hook, Right-Hook, Left-Uppercut',
+                'Jab, Cross, Left-Hook, Right-Hook',
+                'Cross, Right-Hook, Left-Uppercut, Left-Hook' 
 
+
+
+    ]
+}
+// console.log(round2.combos)
 
 
 // CACHED ELEMENT REFERENCES-----------------------------------------------------------
 const r1FirstPrintHere = document.getElementById('round1FirstResults')
 const r1SecondPrintHere = document.getElementById('round1SecondResults')
 const r1ThirdPrintHere = document.getElementById('round1ThirdResults')
+const r2PrintHere = document.getElementById('round2Results')
 const generateRound1FirstResults = document.getElementById('generateRound1FirstResults')
 const generateRound1SecondResults = document.getElementById('generateRound1SecondResults')
 const generateRound1ThirdResults = document.getElementById('generateRound1ThirdResults')
+const generateRound2Results = document.getElementById('generateRound2Results')
 
 
 // EVENT LISTENERS--------------------------------------------------------------------------
@@ -38,6 +53,7 @@ const generateRound1ThirdResults = document.getElementById('generateRound1ThirdR
 generateRound1FirstResults.addEventListener('click', renderRound1First)
 generateRound1SecondResults.addEventListener('click', renderRound1Second)
 generateRound1ThirdResults.addEventListener('click', renderRound1Third)
+generateRound2Results.addEventListener('click', renderRound2)
 
 
 
@@ -63,5 +79,8 @@ function renderRound1Third(){
     r1ThirdPrintHere.innerHTML = `The third part of round one is ${round1.combos[randomizeRound(round1)]}.`
 }
 
+function renderRound2(){
+    r2PrintHere.innerHTML = `Round two is ${round2.combos[randomizeRound(round2)]}.`
+}
 
 
